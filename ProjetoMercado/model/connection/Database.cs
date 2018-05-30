2f5a22a9b3323353c5bccd00240dd935abe6bae0
Leonardo Locatti <locatti.leonardo@gmail.com>
2018-05-30 16:49:09 -0300

    Correções em Database

--- ProjetoMercado/model/connection/Database.cs
+++ ProjetoMercado/model/connection/Database.cs
@@ -14,7 +14,7 @@ namespace ProjetoMercado.model.connection
         private static MySqlConnection connection; /* Conexão com o Banco de Dados */
         private static Database instance; /* Instância do objeto Database */
         private string connectionString = /* String de configuração da Conexão */
-            "Server=localhost; database=mercado; Uid=root; Pwd=leonardobd";
+            "Server=localhost; database=mercado; Uid=root; Pwd=";
 
         /* Construtor privado */
         private Database()
